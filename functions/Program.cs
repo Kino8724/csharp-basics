527e849bac2f6b593a13299505ca4a6d3b92c9d1
Wyatt Ptak <ptakwyatt@gmail.com>
2024-05-07 15:23:51 -0700

    Added Functions code and comments

---
+++ functions/Program.cs
@@ -0,0 +1,11 @@
+﻿// Functions can be public or private (main two)
+// They can be static which means they dont need to be initialized to be used
+// They need a return type, a name, and args if you need them
+// [public || private] [static] <return type> <NameInPascalCase>([<arg type> <arg nameInCamelCase>])
+
+static int AddTwoNums(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+Console.WriteLine(AddTwoNums(1,2));
